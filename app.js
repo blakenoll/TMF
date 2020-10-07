4a71fd1e8200860b718d858442685cd74abcb8d8
Jonathan Stuckey <jonathan.w.stuckey@gmail.com>
2020-10-07 10:01:00 -0700

    Try plotting a chapter

--- app.js
+++ app.js
@@ -4,5 +4,28 @@ function initMap() {
          zoom: 5,
          center: { lat: 37.0902, lng: -95.7129 },
      });
+
+    // Load the stores GeoJSON onto the map.
+    map.data.loadGeoJson('chapters.json', {idPropertyName: 'chapterid'});
+
+    const apiKey = 'AIzaSyAwy6PKU4r7ICmQVsW-e0Pu0-tA9b9MpGM';
+    const infoWindow = new google.maps.InfoWindow();
+
+    // Show the information for a store when its marker is clicked.
+    map.data.addListener('click', (event) => {
+        const category = event.feature.getProperty('category');
+        const name = event.feature.getProperty('name');
+        const description = event.feature.getProperty('description');
+        const position = event.feature.getGeometry().get();
+        const content = `
+        <h2>${name}</h2><p>${description}</p>
+        <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
+        `;
+
+        infoWindow.setContent(content);
+        infoWindow.setPosition(position);
+        infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+        infoWindow.open(map);
+  });
  
  }
